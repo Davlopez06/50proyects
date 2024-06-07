b72c856120eee4050cedfeafe18466ef47d8e278
Davlopez06 <davidlopezvaldes@gmail.com>
2024-06-07 17:28:58 -0500

    [FEATURE] new pregress

--- Progress Steps/js/app.js
+++ Progress Steps/js/app.js
@@ -1,15 +1,55 @@
-var handleClick = function (card) {
+var step = 1;
+var classRemove = function (selector) {
+    var selectorClass = selector.className.split(" ");
+    if (selector.className.includes("".concat(selectorClass[0], "-active"))) {
+        selector.classList.remove("".concat(selectorClass[0], "-active"));
+    }
+};
+var classAdd = function (selector) {
+    var selectorClass = selector.className.split(" ");
+    if (!selector.className.includes("".concat(selectorClass[0], "-active"))) {
+        selector.classList.add("".concat(selectorClass[0], "-active"));
+    }
+};
+var validateButtons = function (selectorChildren) {
+    if (step <= 1) {
+        classRemove(selectorChildren[0]);
+    }
+    else {
+        classAdd(selectorChildren[0]);
+    }
+    if (step >= 4) {
+        classRemove(selectorChildren[1]);
+    }
+    else {
+        classAdd(selectorChildren[1]);
+    }
+};
+var validate = function () {
     if (typeof document !== "undefined" || document) {
-        var slider = document.querySelector(".cards-ctn");
-        if (slider && slider.children) {
-            for (var i = 0; i < slider.children.length; i++) {
-                if (i === card - 1) {
-                    slider.children[i].classList.add("cards-card-active");
+        var step_ctn = document.querySelector(".steps-ctn");
+        var butotons_ctn = document.querySelector(".buttons-ctn");
+        if (step_ctn && step_ctn.children) {
+            var steps = step + (step - 1) * 1;
+            for (var i = 0; i < step_ctn.children.length; i++) {
+                if (i < steps) {
+                    classAdd(step_ctn.children[i]);
                 }
                 else {
-                    slider.children[i].classList.remove("cards-card-active");
+                    classRemove(step_ctn.children[i]);
                 }
             }
         }
+        if (butotons_ctn && butotons_ctn.children) {
+            validateButtons(butotons_ctn.children);
+        }
     }
+};
+var handleClickPrev = function () {
+    step--;
+    validate();
+};
+var handleClickNext = function () {
+    step++;
+    validate();
 };
--- Progress Steps/ts/app.ts
+++ Progress Steps/ts/app.ts
@@ -1,14 +1,59 @@
-const handleClick = (card: number) => {
+let step = 1;
+
+const classRemove = (selector) => {
+  const selectorClass = selector.className.split(" ");
+  if (selector.className.includes(`${selectorClass[0]}-active`)) {
+    selector.classList.remove(`${selectorClass[0]}-active`);
+  }
+};
+
+const classAdd = (selector) => {
+  const selectorClass = selector.className.split(" ");
+  if (!selector.className.includes(`${selectorClass[0]}-active`)) {
+    selector.classList.add(`${selectorClass[0]}-active`);
+  }
+};
+
+const validateButtons = (selectorChildren) => {
+  if (step <= 1) {
+    classRemove(selectorChildren[0]);
+  } else {
+    classAdd(selectorChildren[0]);
+  }
+
+  if (step >= 4) {
+    classRemove(selectorChildren[1]);
+  } else {
+    classAdd(selectorChildren[1]);
+  }
+};
+
+const validate = () => {
   if (typeof document !== "undefined" || document) {
-    const slider = document.querySelector(".cards-ctn");
-    if (slider && slider.children) {
-      for (let i = 0; i < slider.children.length; i++) {
-        if (i === card - 1) {
-          slider.children[i].classList.add("cards-card-active");
+    const step_ctn = document.querySelector(".steps-ctn");
+    const butotons_ctn = document.querySelector(".buttons-ctn");
+    if (step_ctn && step_ctn.children) {
+      const steps = step + (step - 1) * 1;
+      for (let i = 0; i < step_ctn.children.length; i++) {
+        if (i < steps) {
+          classAdd(step_ctn.children[i]);
         } else {
-          slider.children[i].classList.remove("cards-card-active");
+          classRemove(step_ctn.children[i]);
         }
       }
     }
+    if (butotons_ctn && butotons_ctn.children) {
+      validateButtons(butotons_ctn.children);
+    }
   }
+};
+
+const handleClickPrev = () => {
+  step--;
+  validate();
+};
+
+const handleClickNext = () => {
+  step++;
+  validate();
 };
